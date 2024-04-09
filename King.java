fc9405b2a6de28c4900850686e017a08dd2da989
Devil-Boy-10 <164322332+Devil-Boy-10@users.noreply.github.com>
2024-04-09 23:05:25 +0530

    Update King.java

--- King.java
+++ King.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+public class White_Devil
+{
+    public static void main(String args[])
+     {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter Hi ");
+         String n = sc.nextLine();
+         if(n == "Hi")
+           System.out.println("Hello");
+         else
+           System.exit();
+     }
+}
