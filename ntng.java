fa28c3d8f451cb675e236e7dc3ae178c079f739c
Devil-Boy-10 <164322332+Devil-Boy-10@users.noreply.github.com>
2024-04-10 14:57:39 +0530

    Update ntng.java

--- ntng.java
+++ ntng.java
@@ -5,3 +5,30 @@ public class ntng
        {
           Scanner sc = new Scanner(System.in);
           System.out.println("********** Menu **********");
+          System.out.println("     1 : Find area        ");
+          System.out.println("     2 : Find volume      ");
+          System.out.println();
+          System.out.println("  Enter ur choice 1 / 2   ");
+          int c = sc.nextInt();
+          switch(c)
+           {    
+               case 1:
+                         System.out.println("Enter radius");
+                         int r = sc.nextInt();
+                         double l = 3.14*r*r;
+                         System.out.println(l);
+                         break;
+               case 2:
+                         System.out.println("Enter radius");
+                         int ra = sc.nextInt();
+                         System.out.println("Enter height");
+                         int h = sc.nextInt();
+                         double v = 3.14 * ra * ra * h ;
+                         System.out.println(v);
+                         break;
+              default:
+                         System.out.println(" Invalid Option ");
+           }
+        sc.close();
+       }
+}
