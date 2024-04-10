80df1725f7ee0a74fe88912d0275c84fdc418080
Devil-Boy-10 <164322332+Devil-Boy-10@users.noreply.github.com>
2024-04-10 14:32:53 +0530

    Update ntng.java

--- ntng.java
+++ ntng.java
@@ -1,1 +1,7 @@
-ff
+import java.util.*;
+public class ntng
+{
+     public static void main(String args[]);
+       {
+          Scanner sc = new Scanner(System.in);
+          System.out.println("********** Menu **********");
