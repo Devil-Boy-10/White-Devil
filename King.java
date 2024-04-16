c6c723af063617ad3d340a5a4353106d9096796b
Devil-Boy-10 <164322332+Devil-Boy-10@users.noreply.github.com>
2024-04-16 12:06:19 +0530

    Update King.java

--- King.java
+++ King.java
@@ -4,7 +4,7 @@ public class White_Devil
     public static void main(String args[])
      {
          Scanner sc = new Scanner(System.in);
-         System.out.println("Enter Hi ");
+         System.out.println("/tEnter Hi ");
          String n = sc.nextLine();
          if(n == "Hi")
            System.out.println("Hello");
